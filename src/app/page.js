f0cedcfa576dd6b11bed89d7e19676cbe110e34b
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2024-05-30 20:52:35 +0600

    hello world

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { readStreamableValue } from "ai/rsc";
 import Link from "next/link";
 import ContextApi from "@/ContextApi";
 
+export const dynamic = "force-dynamic";
 export default function Home() {
   const {
     suggested,
